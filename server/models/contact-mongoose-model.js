7cf79268e81d63a3101efb79e9181dae08b5fdbf
Augusto Falcao <falcaoaugustos@gmail.com>
2017-11-21 23:57:17 -0300

    feat (contact): create contact mongoose model

---
+++ server/models/contact-mongoose-model.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const ContactSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true,
+    match: [/^[1-9][0-9]{9}$/, 'The value of path {PATH} ({VALUE}) is not a valid mobile number.']
+  },
+  country: {
+    type: String,
+    required: false
+  }
+});
+
+ContactSchema.statics = {
+  get(id) {
+    return this.findById(id)
+      .exec();
+  },
+  list({ skip = 0, limit = 50 } = {}) {
+    return this.find()
+      .skip(+skip)
+      .limit(+limit)
+      .exec();
+  }
+};
+
+export default mongoose.model('Contact', ContactSchema);
