9b8f809b6b37909d38f3587242bcd0d874e721e4
Augusto Falcao <falcaoaugustos@gmail.com>
2017-11-22 01:16:54 -0300

    feat (contact): create contact controller

---
+++ server/controllers/contact-controller.js
@@ -0,0 +1,57 @@
+import Contact from '../models/contact-mongoogse-model';
+
+function load(req, res, next, id) {
+  Contact.get(id)
+    .then((contact) => {
+      req.contact = contact;
+      return next();
+    })
+    .catch(e => next(e));
+}
+
+function get(req, res) {
+  return res.json(req.contact);
+}
+
+function create(req, res, next) {
+  const contact = new Contact({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    phone: req.body.phone,
+    email: req.body.email,
+    country: req.body.country
+  });
+
+  contact.save()
+    .then(savedUser => res.json(savedUser))
+    .catch(e => next(e));
+}
+
+function update(req, res, next) {
+  const contact = req.contact;
+  contact.first_name = req.body.first_name,
+  contact.last_name = req.body.last_name,
+  contact.phone = req.body.phone,
+  contact.email = req.body.email,
+  contact.country = req.body.country
+
+  contact.save()
+    .then(savedContact => res.json(savedContact))
+    .catch(e => next(e));
+}
+
+function list(req, res, next) {
+  const { limit = 50, skip = 0 } = req.query;
+  Contact.list({ limit, skip })
+    .then(contacts => res.json(contacts))
+    .catch(e => next(e));
+}
+
+function remove(req, res, next) {
+  const contact = req.contact;
+  contact.remove()
+    .then(deletedContacts => res.json(deletedContacts))
+    .catch(e => next(e));
+}
+
+export default { load, get, create, update, list, remove };
