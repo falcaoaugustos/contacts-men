32d0fa2b16fbc46ff6fea60bddf2c66de68985da
Augusto Falcão <falcaoaugustos@gmail.com>
2017-11-23 23:52:37 -0300

    feat (db): configure connection to database

--- server/index.js
+++ server/index.js
@@ -1,3 +1,12 @@
+const mongoose = require('mongoose');
+
+const mongoUri = "localhost:27042";
+
+mongoose.connect(mongoUri, { server: { socketOptions: { keepAlive: 1 } } });
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`);
+});
+
 const app = require('./routes/app');
 
 app.listen(3000, _ => console.log('Listening on the port 3000...'));
